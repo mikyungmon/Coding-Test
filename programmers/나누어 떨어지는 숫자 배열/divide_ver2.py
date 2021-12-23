74d8208c178287e4cc44bc299e1b747d0392d820
Mi Kyung Lee <66320010+mikyungmon@users.noreply.github.com>
2021-12-23 17:00:43 +0900

    Delete programmers/나누어 떨어지는 숫자 배열 directory

--- programmers/나누어 떨어지는 숫자 배열/divide_ver2.py
+++
@@ -1,3 +0,0 @@
-# or 앞이 참일경우 해당 값까지만 , 거짓일경우 뒤에 것까지 호출
-# sort랑 sorted차이는 원래 리스트 자체를 바꾸는지 아닌지(sort는 원래 배열도 바꿔버림, sorted는 원래 배열은 그대로)
-def solution(arr, divisor): return sorted([n for n in arr if n%divisor == 0]) or [-1]
