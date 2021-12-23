e32ee4fc60031db8864f7cc3c82ca44e7a876a56
Mi Kyung Lee <66320010+mikyungmon@users.noreply.github.com>
2021-12-23 17:03:30 +0900

    Delete programmers/정수 내림차순으로 배치하기 directory

--- programmers/정수 내림차순으로 배치하기/sort_reverse.py
+++
@@ -1,7 +0,0 @@
-def solution(n):
-    n = list(map(int, str(
-        n)))  # map() : 첫번째 매개변수는 함수, 두번째는 반복 가능한 자료형(str(n)을 int형으로 바꾸고 list화한다는 의미) -> n = [1, 1, 8, 3, 7, 2]
-    n.sort(reverse=True)
-    result = ''.join(map(str, n))
-
-    return int(result)
