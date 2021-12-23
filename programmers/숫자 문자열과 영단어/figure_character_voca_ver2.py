4c824ea04ed73671e5a818420750592c7cfdf7b7
Mi Kyung Lee <66320010+mikyungmon@users.noreply.github.com>
2021-12-23 17:02:43 +0900

    Delete programmers/숫자 문자열과 영단어 directory

--- programmers/숫자 문자열과 영단어/figure_character_voca_ver2.py
+++
@@ -1,7 +0,0 @@
-def solution(s):
-    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-
-    for i in range(len(words)):
-        s = s.replace(words[i], str(i))
-
-    return int(s)
