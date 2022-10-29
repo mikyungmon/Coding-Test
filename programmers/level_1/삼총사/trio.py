236b6745cebe67c2959470ef5c866f47f1e95678
Mi Kyung Lee <66320010+mikyungmon@users.noreply.github.com>
2022-10-30 04:07:08 +0900

    Add files via upload

---
+++ programmers/level_1/삼총사/trio.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+
+
+def solution(number):
+    combi = list(combinations(number, 3))
+    trio = 0
+
+    for i in range(len(combi)):
+        if sum(combi[i]) == 0:
+            trio += 1
+
+    return trio
